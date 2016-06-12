df7390a923b50c96f51f380c99e6dfc57846652b
erudite122 <erudite122@gmail.com>
2016-06-12 10:59:02 -0700

    intial simple task system project
    attached

---
+++ SimpleTaskSystem.Application/SimpleTaskSystemApplicationModule.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using Abp.AutoMapper;
+using Abp.Modules;
+
+namespace SimpleTaskSystem
+{
+    [DependsOn(typeof(SimpleTaskSystemCoreModule), typeof(AbpAutoMapperModule))]
+    public class SimpleTaskSystemApplicationModule : AbpModule
+    {
+        public override void Initialize()
+        {
+            IocManager.RegisterAssemblyByConvention(Assembly.GetExecutingAssembly());
+        }
+    }
+}
